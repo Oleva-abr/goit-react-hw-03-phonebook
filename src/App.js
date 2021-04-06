13c7612a639166eeee4435cf56b24ffb75caa61e
Olena Abramova <olena.v.abramova@gmail.com>
2021-04-06 19:55:08 +0300

    add local storage

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import { v4 as uuidv4 } from 'uuid';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -50,6 +50,19 @@ class App extends Component {
   changeFilter = e => {
     this.setState({ filter: e.currentTarget.value });
   };
+
+  componentDidMount() {
+    const localHistiry = localStorage.getItem('contacts');
+    if (localHistiry !== null) {
+      this.setState({ contacts: JSON.parse(localHistiry) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     return (
